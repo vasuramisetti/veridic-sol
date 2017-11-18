7ccfd5e8656e83ef857bd14b3759c3b1c0c856ce
Vasudev Ramisetti <vasu123689@gmail.com>
2017-11-18 11:55:21 -0500

    vasudev ramisetti

--- 1.java
+++ 1.java
@@ -1,4 +1,7 @@
-<<<<<<< HEAD
+
+
 i'm not vasudev 
-=======
->>>>>>> parent of 507666e... adding 1,2 java files
+
+
+
+
--- 2.java
+++ 2.java
@@ -1,5 +1,9 @@
-<<<<<<< HEAD
+
+
 i'm not sai  
-=======
-dding file 
->>>>>>> parent of 507666e... adding 1,2 java files
+
+
+
+
+
+
